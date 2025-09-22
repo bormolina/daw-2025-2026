4538771912f23fee12125d39d49f46616f51eb32
Borja Molina Zea <5804759+bormolina@users.noreply.github.com>
2025-09-22 10:47:59 +0200

    Reorganizado el código para el curso 2025-2026

--- Primero/Programación/pruebas/pruebas.py
+++
@@ -1,6 +0,0 @@
-from A import A
-
-a = A(5)
-
-if __name__ == "__main__":
-    print(a.b())
--- Segundo/reloj.js
+++
@@ -1,7 +0,0 @@
-function tick(){
-    reloj.innerText = new Date().toLocaleTimeString()
-}
-
-let reloj = document.getElementById("relojCliente")
-let intervalId = window.setInterval(tick, 500)
-
