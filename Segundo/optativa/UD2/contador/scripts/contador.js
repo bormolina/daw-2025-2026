09e9f408e378ec65eac7400f52789d119492f68b
Borja Molina Zea <5804759+bormolina@users.noreply.github.com>
2025-10-01 17:59:04 +0200

    aplacar los lloros

---
+++ Segundo/optativa/UD2/contador/scripts/contador.js
@@ -0,0 +1,37 @@
+const contador = document.querySelector("#contador");
+const boton_sumar = document.querySelector("#boton_sumar");
+const boton_restar = document.querySelector("#boton_restar");
+const boton_contar = document.querySelector("#boton_contar");
+const disabled_ui = [boton_sumar, boton_restar, boton_contar];
+const ring_ring = document.querySelector("#ring_ring");
+
+boton_sumar.addEventListener("click", function(){
+    ring_ring.style.visibility = "hidden";
+    let valor_contador = parseInt(contador.innerHTML);
+    valor_contador += 1;
+    contador.innerHTML = valor_contador;
+})
+
+boton_restar.addEventListener("click", function(){
+    let valor_contador = parseInt(contador.innerHTML);
+    if(valor_contador>0){
+            valor_contador -= 1;
+        contador.innerHTML = valor_contador;
+    }
+})
+
+boton_contar.addEventListener("click", function(){
+    let valor_contador = parseInt(contador.innerHTML);
+    disabled_ui.map(x=>x.disabled = true);
+    if(valor_contador>0){
+        const id_clock = setInterval(function(){
+            valor_contador -= 1;
+            contador.innerHTML = valor_contador;
+            if(valor_contador===0){
+                clearInterval(id_clock);
+                ring_ring.style.visibility = "visible";
+                disabled_ui.map(disabled_ui.map(x=>x.disabled = false));
+            }
+        }, 1000);
+    }
+})
