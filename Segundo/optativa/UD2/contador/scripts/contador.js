c5a9285c607a44cd4b2661c7764a39507ed68f75
Borja Molina Zea <5804759+bormolina@users.noreply.github.com>
2025-10-02 10:55:36 +0200

    Estilos añadidos

--- Segundo/optativa/UD2/contador/scripts/contador.js
+++ Segundo/optativa/UD2/contador/scripts/contador.js
@@ -6,30 +6,32 @@ const disabled_ui = [boton_sumar, boton_restar, boton_contar];
 const ring_ring = document.querySelector("#ring_ring");
 
 boton_sumar.addEventListener("click", function(){
-    ring_ring.style.visibility = "hidden";
     let valor_contador = parseInt(contador.innerHTML);
     valor_contador += 1;
     contador.innerHTML = valor_contador;
+    ring_ring.classList.remove("show");
 })
 
 boton_restar.addEventListener("click", function(){
     let valor_contador = parseInt(contador.innerHTML);
     if(valor_contador>0){
             valor_contador -= 1;
-        contador.innerHTML = valor_contador;
+            contador.innerHTML = valor_contador;
     }
 })
 
 boton_contar.addEventListener("click", function(){
     let valor_contador = parseInt(contador.innerHTML);
-    disabled_ui.map(x=>x.disabled = true);
+    if(valor_contador > 0){
+        disabled_ui.map(x=>x.disabled = true);
+    }
     if(valor_contador>0){
         const id_clock = setInterval(function(){
             valor_contador -= 1;
             contador.innerHTML = valor_contador;
             if(valor_contador===0){
                 clearInterval(id_clock);
-                ring_ring.style.visibility = "visible";
+                ring_ring.classList.add("show");
                 disabled_ui.map(disabled_ui.map(x=>x.disabled = false));
             }
         }, 1000);
