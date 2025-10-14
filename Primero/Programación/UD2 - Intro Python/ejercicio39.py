adb15c4d37d8a41bcde8df56e73ac3ccd0c8204f
bmolzea <bomolzea722@g.educaand.es>
2025-10-14 21:21:50 +0200

    Ejercicio 39 al gusto de mis alumnos

--- Primero/Programación/UD2 - Intro Python/ejercicio39.py
+++ Primero/Programación/UD2 - Intro Python/ejercicio39.py
@@ -1,5 +1,12 @@
-menor = int(input("Introduce un número: "))
-mayor = int(input("Introduce otro número: "))
+num1 = int(input("Introduce un número: "))
+num2 = int(input("Introduce otro número: "))
+
+if num1>num2:
+    mayor = num1
+    menor = num2
+else:
+    mayor = num2
+    menor = num1
 
 # No incluyendo el primer ni último número
 for i in range(menor+1, mayor):
