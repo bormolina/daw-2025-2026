70a36d88c47b61df2cd881abe8818a0f5e8da931
bmolzea <bomolzea722@g.educaand.es>
2025-10-23 19:47:26 +0200

    Actualizado el ejercicio 59

--- Primero/Programación/UD2 - Intro Python/ejercicio59.py
+++ Primero/Programación/UD2 - Intro Python/ejercicio59.py
@@ -1,3 +1,14 @@
 import aleatorio
 
+# Prueba que funciona lanzar moneda
+aleatorio.lanzarMoneda()
+
+# Pruebo que funciona lanzar monedaS
 aleatorio.lanzarMonedas(10)
+
+# Pruebo que funciona lanzar dado
+aleatorio.lanzarDado()
+
+# Pruebo que funciona lanzar dadoS
+aleatorio.lanzarDados(10)
+
