0a3a397ad73a5356ce2957f6b3bc52b02ca32b20
bmolzea <bomolzea722@g.educaand.es>
2025-11-21 20:21:16 +0100

    tres en raya terminado

--- Primero/Programación/UD3 - Listas/2025-2026/tres-en-raya/funcs.py
+++ Primero/Programación/UD3 - Listas/2025-2026/tres-en-raya/funcs.py
@@ -43,4 +43,29 @@ def ia(tablero: list[list[int]]):
     
     tablero[fila][col] = -1
 
+
+# Devuelvo un resumen del tablero
+def resumen(tablero: list[list[int]]) -> tuple[int]:
+    return (
+        sum(tablero[0]),
+        sum(tablero[1]),
+        sum(tablero[2]),
+        sum((tablero[0][0], tablero[1][0], tablero[2][0])),
+        sum((tablero[0][1], tablero[1][1], tablero[2][1])),
+        sum((tablero[0][2], tablero[1][2], tablero[2][2])),
+        sum((tablero[0][0], tablero[1][1], tablero[2][2])),
+        sum((tablero[0][2], tablero[1][1], tablero[2][0]))
+    )
+
+# Devuelve 1 si ha ganado el jugador
+# -1 si gana la ia
+# 0 si la partida continua
+def estado(tablero: list[list[int]]) -> int:
+    datos_resumen = resumen(tablero)
+    if max(datos_resumen) == 3:
+        return 1
+    elif min(datos_resumen) == -3:
+        return -1
+    else: 
+        return 0
     
--- Primero/Programación/UD3 - Listas/2025-2026/tres-en-raya/main.py
+++ Primero/Programación/UD3 - Listas/2025-2026/tres-en-raya/main.py
@@ -1,4 +1,4 @@
-from funcs import imprimir_tablero, insertar_jugada, ia
+from funcs import imprimir_tablero, insertar_jugada, ia, estado
 
 tablero = [
     [0, 0, 0],
@@ -7,10 +7,29 @@ tablero = [
 ]
 
 imprimir_tablero(tablero)
+turno = 0
 
 while True:
     insertar_jugada(tablero)
     imprimir_tablero(tablero)
+    
+    estado_partida = estado(tablero)
+    if estado_partida == 1:
+        print("HAS GANADO!!!!")
+        break
+
+    if turno == 5:
+        print("Has empatado")
+    
+    input("La IA juega...")
     ia(tablero)
-    input("La IA juega: ")
+
+    estado_partida = estado(tablero)
     imprimir_tablero(tablero)
+
+    if estado_partida == -1:
+        print("HAS PERDIDO!!!!")
+        break
+
+    turno += 1
+
