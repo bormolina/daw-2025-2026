05c8d54b8e6d6f44a263730782615f094099b195
bmolzea <bomolzea722@g.educaand.es>
2026-03-03 20:51:09 +0100

    PROG - UD5 - Aclaraciones correción del examen

--- Primero/Programación/UD5 - Clases/examen/ej4.py
+++ Primero/Programación/UD5 - Clases/examen/ej4.py
@@ -4,6 +4,7 @@ productos = get_productos()
 
 resultado = {}
 
+# Calculo el número de productos y la sumatoria
 for p in productos:
     for cat in p.categorias:
         datos_cat = resultado.get(cat, {"num_productos": 0, "precio_medio": 0.0})
@@ -11,9 +12,11 @@ for p in productos:
         datos_cat["precio_medio"] += p.precio
         resultado[cat] = datos_cat
 
+# Calculo la media
 for cat in resultado:
     num = resultado[cat]["num_productos"]
     resultado[cat]["precio_medio"] = resultado[cat]["precio_medio"] / num
 
+# Muestro los resultado
 for cat in resultado:
     print(f"{cat} -- Número productos: {resultado[cat]['num_productos']} -- Precio medio: {resultado[cat]['precio_medio']:.2f}€")
