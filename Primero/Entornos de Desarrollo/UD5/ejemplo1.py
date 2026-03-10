43eea1882c7d6738da61ce9ae25ca3d52e26a971
Borja Molina Zea <5804759+bormolina@users.noreply.github.com>
2026-03-10 11:23:16 +0100

    Entornos - UD5 - Depuración

---
+++ Primero/Entornos de Desarrollo/UD5/ejemplo1.py
@@ -0,0 +1,43 @@
+class CuentaBancaria:
+
+    def __init__(self, titular: str, saldo: float) -> None:
+        self.titular: str = titular
+        self.saldo: float = saldo
+        self.movimientos: list[float] = []
+
+    def ingresar(self, cantidad: float) -> None:
+        self.saldo += cantidad
+        self.movimientos.append(self.saldo)  
+
+    def retirar(self, cantidad: float) -> None:
+        if cantidad > self.saldo:
+            print("Fondos insuficientes")
+        else:
+            self.saldo -= cantidad
+            self.movimientos.append(-cantidad)
+
+    def calcular_media_movimientos(self) -> float:
+        total: float = 0
+
+        for cantidad in self.movimientos:
+            total += cantidad
+
+        media = total / len(self.movimientos)
+        return media
+
+
+if __name__ == "__main__":
+
+    cuenta = CuentaBancaria("Borjoso", 1000)
+
+    cuenta.ingresar(200)
+    cuenta.retirar(150)
+    cuenta.ingresar(300)
+
+    cuenta.mostrar_movimientos()
+
+    print(f"Saldo actual: {cuenta.saldo}")
+
+    media = cuenta.calcular_media_movimientos()
+    print(f"Media de movimientos: {media:.2f}")
+
