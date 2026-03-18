b02fa3b3d4f1da8196447ca02aa8dd8a9fa02910
bmolzea <bomolzea722@g.educaand.es>
2026-03-18 16:31:27 +0100

    PROG - UD7 - Pequeñas modificaciones en ejemplo 2 y 3

--- Primero/Programación/UD7 - E-S/ejemplo2.py
+++ Primero/Programación/UD7 - E-S/ejemplo2.py
@@ -1,11 +1,13 @@
 # Leer un fichero de texto línea a línea y contar cuántas líneas empiezan por la letra a
 
-contador = 0
+contador_lineas = 0
+contador_a = 0
 
 with open("quijote.txt", "r", encoding="utf-8") as f:
     for linea in f:
+        contador_lineas += 1
         if linea[0].lower() == "a":
-            contador += 1
+            contador_a += 1
 
 
-print(f"El Quijote tiene {contador} líneas que empiezan por a")
+print(f"El Quijote tiene un total de {contador_lineas} líneas de las cuales {contador_a} líneas que empiezan por a que representan un {contador_lineas/contador_a} del total")
--- Primero/Programación/UD7 - E-S/ejemplo3.py
+++ Primero/Programación/UD7 - E-S/ejemplo3.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
 # Leer un fichero de texto todas las líneas a la vez y contar cuántas líneas empiezan por la letra a
 
-with open("quijote.txt", "r", encoding="utf-8") as f:
+nombre_fichero = Path(__file__).parent / "quijote.txt"
+
+with open(nombre_fichero, "r", encoding="utf-8") as f:
     lineas = f.readlines()
     contador = len([linea for linea in lineas if linea[0].lower() == "a"])
 
